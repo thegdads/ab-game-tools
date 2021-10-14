9d0f5aa8df686e2f3ed5a769831066d66f838dfb
Yuda Ferry Mahendra <juncyboy@gmail.com>
2021-10-15 00:19:49 +0700

    Fix jsx nested depth

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -3,6 +3,27 @@ import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, Io
 import { useParams } from 'react-router';
 import './Page.css';
 
+const Header: React.FC<{ name: string }> = ({ name }) => {
+  return (
+    <IonHeader collapse="condense">
+      <IonToolbar>
+        <IonTitle size="large">{name}</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  );
+}
+
+const Toolbar: React.FC<{ name: string }> = ({ name }) => {
+  return (
+    <IonToolbar>
+      <IonButtons slot="start">
+        <IonMenuButton />
+      </IonButtons>
+      <IonTitle>{name}</IonTitle>
+    </IonToolbar>
+  )
+}
+
 const Page: React.FC = () => {
 
   const { name } = useParams<{ name: string; }>();
@@ -10,20 +31,11 @@ const Page: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
-        </IonToolbar>
+        <Toolbar name={name} />
       </IonHeader>
 
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <Header name={name} />
       </IonContent>
     </IonPage>
   );
