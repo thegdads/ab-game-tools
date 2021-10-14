c0748aa3643d1cd1154bdf0af72a83b8dcde7873
Yuda Ferry Mahendra <juncyboy@gmail.com>
2021-10-15 00:04:12 +0700

    for deepsource

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
